aade6eaa6f6627a7e64ee8b5b17ec96e37ec3a5b
Yeifer <developer@yeifer.co>
2025-08-27 21:26:53 +0000

    feat: :sparkles: add notification doctype

---
+++ integration_gateway/integration_gateway/doctype/intgw_notification/intgw_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Picurit and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("INTGW Notification", {
+// 	refresh(frm) {
+
+// 	},
+// });
